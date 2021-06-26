681300cdc2054c1ec247ed1f282ac7d190f6e2b4
Fabrice Etanchaud <fabrice.etanchaud@netc.fr>
2021-06-26 16:06:29 +0200

    CORRECTION AFTER DEADLINE

--- dbt/travaux/models/marts/f_availability_snapshot.sql
+++ dbt/travaux/models/marts/f_availability_snapshot.sql
@@ -5,6 +5,8 @@ with periods as (
     {{ ref('f_availability_periods') }}
 )
 
+/*
+
 select d_dates.date_id
   ,count(*) as active_professionals_count
 from {{ ref('d_dates') }} as d_dates
@@ -12,4 +14,18 @@ left join periods on
   periods.begin_date <= d_dates.date_id  and d_dates.date_id < periods.end_date
 where d_dates.date_id between min_begin_date and 20200330
   and periods.availability_status = 'Yes'
+group by d_dates.date_id
+
+***** THIS CORRECTION WAS MADE AFTER DEADLINE *****
+
+*/
+
+select d_dates.date_id
+  ,count(periods.availability_period_id) as active_professionals_count
+from {{ ref('d_dates') }} as d_dates
+left join periods on
+  periods.begin_date <= d_dates.date_id
+  and d_dates.date_id < periods.end_date
+  and periods.availability_status = 'Yes'
+where d_dates.date_id between min_begin_date and 20200330
 group by d_dates.date_id
